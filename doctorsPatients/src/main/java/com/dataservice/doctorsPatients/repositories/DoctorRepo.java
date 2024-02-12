b322d0f40df4f631806f8e7c67bb0873702fdc89
Anton <a.tashchilin@bk.ru>
2024-02-12 09:03:37 +0400

    add  method findByFio

--- doctorsPatients/src/main/java/com/dataservice/doctorsPatients/repositories/DoctorRepo.java
+++ doctorsPatients/src/main/java/com/dataservice/doctorsPatients/repositories/DoctorRepo.java
@@ -1,11 +1,14 @@
 package com.dataservice.doctorsPatients.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dataservice.doctorsPatients.models.doctors.Doctor;
+import com.dataservice.doctorsPatients.models.util.FIODto;
 
 @Repository
 public interface DoctorRepo extends JpaRepository<Doctor,Integer> {
-    
+    Optional<Doctor> findByFio(FIODto fio);
 }
--- doctorsPatients/src/main/java/com/dataservice/doctorsPatients/repositories/PatientRepo.java
+++ doctorsPatients/src/main/java/com/dataservice/doctorsPatients/repositories/PatientRepo.java
@@ -1,11 +1,14 @@
 package com.dataservice.doctorsPatients.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dataservice.doctorsPatients.models.patients.Patient;
+import com.dataservice.doctorsPatients.models.util.FIODto;
 
 @Repository
 public interface PatientRepo extends JpaRepository<Patient,Integer> {
-    
+    Optional<Patient> findByFio(FIODto fio);
 }
