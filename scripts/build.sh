aa5cb47f1bedf2d9e07f767b5b6f235c0325d77d
Anton <a.tashchilin@bk.ru>
2024-02-18 21:04:09 +0400

    add scripts

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+gd=docker
+gr=$(groups "$USER" | grep -o -w "\b$gd\b")
+
+if [ "$gd" = "$gr" ]; then
+    cd $(dirname $0)/.. || exit 1
+    cd doctorsPatients
+    DOCKER_BUILDKIT=1 docker build -f build-elements/Dockerfile -t serv/data_service .
+    cd ..
+    # cd producer
+    # DOCKER_BUILDKIT=1 docker build -f build-elements/Dockerfile -t serv/producer .
+    # cd ..
+else
+    echo "User $USER is not a member of the Docker group."
+    exit 1
+fi
